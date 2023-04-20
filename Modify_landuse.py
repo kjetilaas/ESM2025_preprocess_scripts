9fedbd0597a471cbd8c9ad7392aeaa34485a2474
Kjetil Aas <kjetisaa@login-1.betzy.sigma2.no>
2023-04-20 20:52:37 +0200

    Fixed Modify_landuse.py

--- Modify_landuse.py
+++ Modify_landuse.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 import xarray as xr
-import netCDF4 as nc4
+import netCDF4 as nc
 import subprocess
 import os
 import Plot_LU
 
 from matplotlib import pyplot as plt
 
-# copy and remap files with separate shell scrip, of not done already
+# copy and remap files with separate shell scrip
 subprocess.call("./CopyAndRemapLU.sh", shell=True)
 
 # define file names
@@ -18,9 +18,9 @@ new_surface_file = '/cluster/work/users/kjetisaa/isimip_forc/Ohter_modified_file
 isimip_LUtotals_file = '/cluster/work/users/kjetisaa/isimip_forc/Ohter_modified_files/landuse-totals_histsoc_1.9x2.5deg_annual_1850_2014.nc'
 
 # load the dataset
-ds_LU = xr.open_dataset(new_landuse_file, decode_times=False)
-ds_surf = xr.open_dataset(new_surface_file, decode_times=False)
-ds_isimip = xr.open_dataset(isimip_LUtotals_file, decode_times=False)
+ds_LU = nc.Dataset(new_landuse_file, mode='r+')
+ds_surf = nc.Dataset(new_surface_file, mode='r+')
+ds_isimip = nc.Dataset(isimip_LUtotals_file, mode='r')
 
 # plot Crop maps and data series 
 Plot_LU.plot_land_use_data(ds_LU, ds_isimip, 0, 'Crop_maps' )
@@ -28,23 +28,22 @@ Plot_LU.plot_land_use_data(ds_LU, ds_isimip, 164, 'Crop_maps' )
 
 # map isimip crop types to clm input data
 # so far only using total crop area, so only need to modify PCT_CROP
+
 ds_LUnew=ds_LU
-ds_surfnew=ds_surf
+pct_crop_old=ds_surf['PCT_CROP'][:]
 
-#pct_cropSURF = ds_surfnew['PCT_CROP']   
-#pct_cropLU = ds_LUnew['PCT_CROP']   
+pct_cropISIMIP = ds_isimip['cropland_total'][:]
+pct_cropISIMIPNoNan = np.where(pct_cropISIMIP>1., 0, pct_cropISIMIP)
 
-pct_cropISIMIP = ds_isimip['cropland_total']
-pct_cropISIMIPNoNan = np.where(np.isnan(pct_cropISIMIP), 0, pct_cropISIMIP)
+ds_surf['PCT_CROP'][:]=pct_cropISIMIPNoNan[0,:,:] * 100 
+ds_surf['PCT_NATVEG'][:]=ds_surf['PCT_NATVEG']+pct_crop_old-ds_surf['PCT_CROP'][:]
+ds_LU['PCT_CROP'][0:-1,:,:].values=pct_cropISIMIPNoNan * 100 
+ds_LU['PCT_CROP'][-1,:,:]=ds_LU['PCT_CROP'][-2,:,:]
 
-ds_surfnew['PCT_CROP'].values=pct_cropISIMIPNoNan[0,:,:] * 100 
-ds_surfnew['PCT_NATVEG']=ds_surfnew['PCT_NATVEG']+ds_surf['PCT_CROP'].values-ds_surfnew['PCT_CROP'].values
-ds_LUnew['PCT_CROP'][0:-1,:,:].values=pct_cropISIMIPNoNan * 100 
-ds_LUnew['PCT_CROP'][-1,:,:]=ds_LUnew['PCT_CROP'][-2,:,:]
+# Plot modified files
+Plot_LU.plot_land_use_data(ds_LU, ds_isimip, 0, 'Crop_maps_AfterModify' )
+Plot_LU.plot_land_use_data(ds_LU, ds_isimip, 164, 'Crop_maps_AfterModify' ) 
 
 # Save the modified NetCDF file
-ds_LUnew.to_netcdf(new_landuse_file)
-ds_surfnew.to_netcdf(new_surface_file)
-
-Plot_LU.plot_land_use_data(ds_LUnew, ds_isimip, 0, 'Crop_maps_AfterModify' )
-Plot_LU.plot_land_use_data(ds_LUnew, ds_isimip, 164, 'Crop_maps_AfterModify' ) 
+ds_LU.close()
+ds_surf.close()
--- Plot_LU.py
+++ Plot_LU.py
@@ -3,16 +3,16 @@ import xarray as xr
 import matplotlib.pyplot as plt
 
 def plot_land_use_data(ds1, ds2, t, figname_in):
-    filename='Figures/' + figname_in  + str(1850+t) +  '.png'
+    filename='Figures/' + figname_in + '_' + str(1850+t) +  '.png'
     # CLM LU data
-    pct_crop1 = ds1['PCT_CROP']    
-    lat1 = ds1['LATIXY']
-    lon1 = ds1['LONGXY']
-    area = ds1['AREA']
+    pct_crop1 = ds1['PCT_CROP'][:]    
+    lat1 = ds1['LATIXY'][:]
+    lon1 = ds1['LONGXY'][:]
+    area = ds1['AREA'][:]
     # Isimip LU data    
-    pct_crop2 = ds2['cropland_total'] * 100  
-    lat2 = ds2['lat']
-    lon2 = ds2['lon']
+    pct_crop2 = ds2['cropland_total'][:] * 100.  
+    lat2 = ds2['lat'][:]
+    lon2 = ds2['lon'][:]
 
     # Create figure
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 12), subplot_kw={'projection': 'rectilinear'})
@@ -30,7 +30,7 @@ def plot_land_use_data(ds1, ds2, t, figname_in):
     fig.colorbar(im2, ax=axs[0, 1])
 
     # Plot difference on lower left subplot
-    im3 = axs[1, 0].pcolormesh(lon1, lat1, pct_crop2[t,:,:].values-pct_crop1[t,:,:].values, cmap='RdBu_r', vmin=-10, vmax=10)
+    im3 = axs[1, 0].pcolormesh(lon1, lat1, pct_crop2[t,:,:]-pct_crop1[t,:,:], cmap='RdBu_r', vmin=-10, vmax=10)
     axs[1, 0].set_title('Difference (ISIMIP-Default)')
     fig.colorbar(im3, ax=axs[1, 0])
 
